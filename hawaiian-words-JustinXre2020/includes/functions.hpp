0f2adc91fb76cba2dc7ec41d2fbdefafa88d07c0
Justin Xiao <66919694+JustinXre2020@users.noreply.github.com>
2022-02-02 11:20:54 -0600

    Add files via upload

---
+++ hawaiian-words-JustinXre2020/includes/functions.hpp
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_HPP
+#define FUNCTIONS_HPP
+#include <string>
+#include <vector>
+// Your function declarations should go in this header file.
+std::string ConvertHawaiian(std::string str, std::vector<char> v,
+                            std::string phonetics);
+
+#endif
