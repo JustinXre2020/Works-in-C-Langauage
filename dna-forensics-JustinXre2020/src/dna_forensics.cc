1fa318e6a598b30edee98d9ca508e2153824d7e4
Justin Xiao <66919694+JustinXre2020@users.noreply.github.com>
2022-02-08 23:34:01 -0600

    Update dna_forensics.cc

--- dna-forensics-JustinXre2020/src/dna_forensics.cc
+++ dna-forensics-JustinXre2020/src/dna_forensics.cc
@@ -6,25 +6,6 @@
 #include "functions.hpp"
 #include "utilities.hpp"
 
-// int main() {
-//   std::ifstream ifs{"/home/vagrant/src/mp-dna-forensics-JustinXre2020/tests/ex-str-database-from-prompt.dat"};
-//   std::map<std::string, std::vector<std::string>> map;
-//   for(std::string line; std::getline(ifs, line); line = "") 
-//     map = ConvertDNA(utilities::GetSubstrs(line, ','), map);
-
-//   for (auto const& element : map) {
-//     std::cout << element.first << std::endl;
-//     for (size_t i = 0; i < element.second.size(); ++i) {
-//       std::cout << element.second[i] << std::endl;
-//     }
-//   }
-//   std::cout << FindPerson(map,  "AGACGGGTTACCATGACTATTATTATTATTATTATTATTATACGTATATCGTACGGAGATGAGATGAGATGAGATGAGATGCCTCGACTTCGATCGCAATGAATGCCAATAGACAAAATATTATTAT") << std::endl;
-//   std::cout << FindPerson(map,  "TATTATTATTATAACCCTGCGCGCGCGCGATCCAGCATTAGCTAGCATCAAGATGAGATGAGATGGAATTTCGAAATGAATGAATGAATGAATGAATGAATG") << std::endl;
-//   std::cout << FindPerson(map,  "GGTACAGATGGCAAAGATGAGATGAGATGGTCGTCGAGCAATCGTTTCGATAATGAATGAATGAATGAATGAATGAATGACACACGTCGATGCTAGCGGCGGATCGTATATTATAACCCCTAG") << std::endl;
-//   return 0;
-// }
-
-
 int main(int argc, char* argv[]) {
   if (argc != 3) {
     std::cerr << "Usage: " << argv[0] << " [input_file] [dna_sequence]"
