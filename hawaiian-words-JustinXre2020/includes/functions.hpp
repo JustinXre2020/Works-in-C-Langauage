4b79df236f4aaa681b38e67f7a5591289396b878
Justin Xiao <66919694+JustinXre2020@users.noreply.github.com>
2022-04-13 17:12:01 -0500

    Delete hawaiian-words-JustinXre2020 directory

--- hawaiian-words-JustinXre2020/includes/functions.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTIONS_HPP
-#define FUNCTIONS_HPP
-#include <string>
-#include <vector>
-// Your function declarations should go in this header file.
-std::string ConvertHawaiian(std::string str, std::vector<char> v,
-                            std::string phonetics);
-
-#endif
