7add5af795b2fd44673db04262ef77779b542680
Justin Xiao <66919694+JustinXre2020@users.noreply.github.com>
2022-04-13 17:20:48 -0500

    Add files via upload

---
+++ lab-object-decomposition/includes/hospital.hpp
@@ -0,0 +1,23 @@
+#ifndef HOSPITAL_HPP
+#define HOSPITAL_HPP
+
+#include <set>
+#include <string>
+#include <vector>
+#include "doctor.hpp"
+#include "appointment.hpp"
+
+class Hospital {
+public:
+  Hospital() = default;
+  void AddAppointment(TimeSlot time, std::string patient);
+  void AddDoctor(Doctor& d);
+  Doctor* GetAvailableDoctor(TimeSlot time);
+  void RemoveAppointment(TimeSlot time, std::string patient);
+
+private:
+  std::vector<Doctor*> doctors_;
+  std::vector<Appointment> appointments_;
+};
+
+#endif
