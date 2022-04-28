dfb45221f2c03a19dc076510cc5a96d036589b70
Justin Xiao <66919694+JustinXre2020@users.noreply.github.com>
2022-04-27 23:56:44 -0500

    Add files via upload

--- word-search/src/driver.cc
+++ word-search/src/driver.cc
@@ -5,12 +5,23 @@
 #include "WordSearchSolver.hpp"
 
 int main() {
-  std::vector<std::vector<char>> puzzle{{'c', 'h', 'a', 'o', 't'},
-                                        {'t', 'e', 'l', 'a', 'o'},
-                                        {'a', 'h', 'h', 'o', 'm'},
-                                        {'c', 't', 'a', 'h', 'u'},
-                                        {'y', 'e', 'h', 'o', 'a'}};
+  std::vector<std::vector<char>> puzzle{{'t', 's', 'e', 'e', 'k', 's', 'q', 'h', 'l', 'e'},
+                                        {'a', 'e', 'l', 'a', 'o', 'y', 'w', 'h', 'n', 'd'},
+                                        {'r', 'w', 'h', 'o', 'm', 'y', 'e', 'u', 'j', 'c'},
+                                        {'n', 't', 'a', 'h', 'u', 'g', 'm', 'g', 'h', 'v'},
+                                        {'i', 'e', 'h', 'r', 'a', 'e', 'f', 'j', 'f', 'r'},
+                                        {'s', 'i', 'm', 'h', 'r', 'i', 'l', 'h', 'd', 'f'},
+                                        {'h', 'o', 'n', 'a', 'k', 'i', 't', 'd', 's', 'b'},
+                                        {'e', 'p', 't', 's', 'j', 'p', 'o', 'h', 'e', 't'},
+                                        {'d', 'e', 'q', 'f', 'g', 'k', 'i', 'r', 'z', 'n'},
+                                        {'c', 'g', 'r', 'i', 'n', 'g', 'i', 'n', 'g', 'c'}};
   WordSearchSolver wss(puzzle);
  
-  std::cout << wss.FindWord("ohh");
+  std::cout << wss.FindWord("enumerate") << std::endl;
+
+  std::cout << wss.FindWord("tarnished") << std::endl;
+  std::cout << wss.FindWord("warrior") << std::endl;
+  std::cout << wss.FindWord("seeks") << std::endl;
+  std::cout << wss.FindWord("elden") << std::endl;
+  std::cout << wss.FindWord("ring") << std::endl;
 }
